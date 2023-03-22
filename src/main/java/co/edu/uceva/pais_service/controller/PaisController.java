df31df08e0f79d40138b795530e2f0860eed44f9
Juan Manuel Carvajal Rada <jmcr-@hotmail.com.ar>
2023-03-22 16:38:11 -0500

    se termino de crear el crud del proyecto base

--- src/main/java/co/edu/uceva/pais_service/controller/PaisController.java
+++ src/main/java/co/edu/uceva/pais_service/controller/PaisController.java
@@ -3,10 +3,7 @@ package co.edu.uceva.pais_service.controller;
 import co.edu.uceva.pais_service.model.entities.Pais;
 import co.edu.uceva.pais_service.model.service.IPaisSevice;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,11 +23,34 @@ public class PaisController {
     public String imprimir(@PathVariable("nombre") String nombre){
         return "hola esto es una prueba " +nombre;
     }
+
     @GetMapping("/paises")
     public List<Pais>listar(){
         List <Pais> paises= new ArrayList<>();
         return paisSevice.findAll();
-
-
     }
+
+    @GetMapping("/paises/{id}")
+    public Pais buscarPais(@PathVariable("id") Long id){
+        return paisSevice.findById(id);
+    }
+
+    @PostMapping("/paises")
+    public Pais crearPais(@RequestBody Pais pais){
+        return paisSevice.save(pais);
+    }
+
+    @DeleteMapping("/paises/{id}")
+    public void borrarPais(@PathVariable("id") Long id){
+        Pais pais;
+        pais=paisSevice.findById(id);
+        paisSevice.delete(pais);
+    }
+
+    @PutMapping("/paises")
+    public Pais actualizarPais(@RequestBody Pais pais){
+        return paisSevice.update(pais);
+    }
+
+
 }
--- src/main/java/co/edu/uceva/pais_service/model/service/PaisServiceImpl.java
+++ src/main/java/co/edu/uceva/pais_service/model/service/PaisServiceImpl.java
@@ -13,17 +13,17 @@ public class PaisServiceImpl implements IPaisSevice{
     IPaisDao paisDao;
     @Override
     public Pais save(Pais pais) {
-        return null;
+        return paisDao.save(pais);
     }
 
     @Override
     public void delete(Pais pais) {
-
+        paisDao.delete(pais);
     }
 
     @Override
     public Pais update(Pais pais) {
-        return null;
+        return paisDao.save(pais);
     }
 
     /**
@@ -37,6 +37,6 @@ public class PaisServiceImpl implements IPaisSevice{
 
     @Override
     public Pais findById(Long id) {
-        return null;
+        return paisDao.findById(id).get();
     }
 }
